7440649853a4001e7c2b9909d235605d7117f34f
fuqiangjun01 <17611473176@163.com>
2021-03-28 16:44:50 +0800

    测试ECS

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,30 @@
+﻿/******************************************
+ *Copyright(C) 2021 by #COMPANY#
+ *All rights reserved.
+ *FileName:     #SCRIPTFULLNAME#
+ *Author:       #AUTHOR#
+ *Version:      #VERSION#
+ *UnityVersion: #UNITYVERSION#
+ *Date:         #DATE#
+ *Description:  
+ *History:
+*******************************************/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class NewBehaviourScript : MonoBehaviour 
+{
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
